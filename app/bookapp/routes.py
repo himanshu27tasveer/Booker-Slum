24b5670cd87472590a618ab4f4968e9139307704
Himanshu Mahawar <himanshu27.stu@gmail.com>
2020-07-09 23:21:04 +0530

    Update routes.py

--- app/bookapp/routes.py
+++ app/bookapp/routes.py
@@ -198,13 +198,23 @@ def book_info(id):
 		book_rev = db.execute("SELECT * FROM reviews WHERE book_id=:id",{"id":book_id}).fetchone()
 		booktitle = request.form.get('title')
 		bookrating = request.form.get("stars")
-		bookrating = float(book_rev.rating) + float(bookrating) 
-		bookreview = request.form.get("review")
+		if book_rev.rating or book_rev.review_count is None:
+			bookrating = int(bookrating) 
+			bookreview = request.form.get("review")
 
-		bookreviewcnt = float(book_rev.review_count) + 1
-		db.execute("INSERT INTO reviews (review, user_id, book_id, rating, title, time, review_count) VALUES (:review, :user_id, :book_id, :rating, :title, :time, :review_count)", 
-								{"isbn":bookisbn, "review":bookreview, "user_id":user_id, "book_id":book_id, "rating":bookrating, "title":booktitle, "time":datetime.now(tz_India), "review_count":bookreviewcnt})
-		db.commit()
+			db.execute("INSERT INTO reviews (review, user_id, book_id, rating, title, time, review_count) VALUES (:review, :user_id, :book_id, :rating, :title, :time, 1)", 
+									{"isbn":bookisbn, "review":bookreview, "user_id":user_id, "book_id":book_id, "rating":bookrating, "title":booktitle, "time":datetime.now(tz_India)})
+			db.commit()
+
+		else:
+
+			bookrating = float(book_rev.rating) + int(bookrating) 
+			bookreview = request.form.get("review")
+
+			bookreviewcnt = float(book_rev.review_count) + 1
+			db.execute("INSERT INTO reviews (review, user_id, book_id, rating, title, time, review_count) VALUES (:review, :user_id, :book_id, :rating, :title, :time, :review_count)", 
+									{"isbn":bookisbn, "review":bookreview, "user_id":user_id, "book_id":book_id, "rating":bookrating, "title":booktitle, "time":datetime.now(tz_India), "review_count":bookreviewcnt})
+			db.commit()
 		return redirect("/book_info/" + str(book_id))
 	else:
 		try:
