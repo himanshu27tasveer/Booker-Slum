791f3deadbf4763b6338fc2fef039385bb3efc9f
Himanshu Mahawar <himanshu27.stU@gmail.com>
2020-07-20 13:48:29 +0530

    Favicon added

--- app/bookapp/routes.py
+++ app/bookapp/routes.py
@@ -34,7 +34,9 @@ def login_required(f):
 def static_from_root():
 	return send_from_directory(app.static_folder, request.path[1:])
 
-
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/')
