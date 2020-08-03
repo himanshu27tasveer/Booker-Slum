4f71526cc81f4573a53fd7a988bcfe85f47f088e
Himanshu Mahawar <himanshu27.stU@gmail.com>
2020-08-03 15:21:09 +0530

    Update __init__.py

--- app/bookapp/__init__.py
+++ app/bookapp/__init__.py
@@ -18,6 +18,9 @@ if not os.environ["DATABASE_URL"]:
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_COOKIE_SECURE"] = True
+app.config["SESSION_COOKIE_HTTPONLY"] = True
+app.config["SESSION_COOKIE_SAMESITE"] = 'Strict'
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
